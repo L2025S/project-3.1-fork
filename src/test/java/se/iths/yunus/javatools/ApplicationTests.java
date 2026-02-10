fb2c8e97baa62b68f4a19d22b16e8597c094a9e9
ynsuldg <yns.uldg@hotmail.com>
2026-02-10 10:38:28 +0100

    Initial commit

---
+++ src/test/java/se/iths/yunus/javatools/ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.yunus.javatools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
